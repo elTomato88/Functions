8d6d3844384ad79cd6daabda5b2d21188c70e911
TomatoMorph <TomatoMorph@DESKTOP-P84S43R>
2025-04-22 21:42:02 +0500

    TemplatedTestStatic

--- TemplatedLIBcheck/main.cpp
+++ TemplatedLIBcheck/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "..\Arrays\TemplatesPrint.h"
+#include "..\Arrrays\TemplatesPrint.h"
 
 
 
@@ -8,7 +8,7 @@ int main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
-	int arr[n] = {1,2,3,4,5}
+	int arr[n] = { 1,2,3,4,5 };
 	Print(arr, n);
 	return 0;
 }
